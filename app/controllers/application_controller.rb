6eb2a914f152f4616b7bbfee3144bde242089c81
syun68 <coldsmash2628@gmail.com>
2022-10-03 20:44:29 +0900

    fix application_controller to control session for current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+  before_action :set_current_user
+
+  private
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      flash[:notice] = "ログインが必要です"
+      redirect_to :login
+    end
+  end
+
+  def forbid_login_user
+    if logged_in?
+      flash[:notice] = "すでにログインしています"
+      redirect_to "/"
+    end
+  end
 end
