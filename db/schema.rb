a3c901af21bf140adcd294b9a3483e45b68bd05c
syun68 <coldsmash2628@gmail.com>
2022-10-03 20:50:36 +0900

    fix db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_10_01_233213) do
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "introduction"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
+    t.string "current_password"
+    t.string "image"
+    t.string "password_digest"
+  end
 
 end
