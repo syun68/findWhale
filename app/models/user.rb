df800967d6e791cac5c3df69b1d38f58ec11560c
syun68 <coldsmash2628@gmail.com>
2022-10-03 20:38:32 +0900

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  validates :name, presence:true, uniqueness:true
+  validates :email, presence:true, uniqueness:true
+  validates :password, presence:true, confirmation:true, length:{in: 6..20}
+
+  has_secure_password
+
+  before_create :set_current_password
+  before_update :update_current_password
+
+  def set_current_password
+    self.current_password = password
+  end
+
+  def update_current_password
+    self.current_password = password
+  end
+end
