fd5add2d34fa19097d0dcd31e9191a0d819bc89a
syun68 <coldsmash2628@gmail.com>
2022-10-10 17:36:21 +0900

    add new post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,38 @@
+class PostsController < ApplicationController
+  def index
+    @post = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+      if @post.save
+        flash[:notice] = "目撃情報を投稿しました"
+        redirect_to "/"
+      else
+        render 'posts/new',  status: :unprocessable_entity
+      end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    binding.pry
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :image, :place_prefecture, :place_detail, :description)
+    end
+end
