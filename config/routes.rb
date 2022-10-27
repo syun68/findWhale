3cbabc829a5e8829b5babc99e9126ad8fb529756
syun68 <coldsmash2628@gmail.com>
2022-10-28 04:00:12 +0900

    [update] add routing to post edit page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   get 'posts/index'
-  get    '/',                     to: 'users#top'
-  get    '/login',                to: 'sessions#new'
-  post   '/login',                to: 'sessions#create'
-  delete '/logout',               to: 'sessions#destroy'
-  get    '/users/account',        to: 'users#account'
-  get    '/users/profile',        to: 'users#profile'
-  post   '/users/profile_update', to: 'users#profile_update'
-  get    '/users/edit',           to: 'users#edit'
-  post   '/users/update',         to: 'users#update'
+  get    '/',                               to: 'users#top'
+  get    '/login',                          to: 'sessions#new'
+  post   '/login',                          to: 'sessions#create'
+  delete '/logout',                         to: 'sessions#destroy'
+  get    '/users/account',                  to: 'users#account'
+  get    '/users/profile',                  to: 'users#profile'
+  post   '/users/profile_update',           to: 'users#profile_update'
+  get    '/users/edit',                     to: 'users#edit'
+  post   '/users/update',                   to: 'users#update'
+  get    '/posts/:id/index',                to: 'posts#edit_index'
 
   resources :users
   resources :posts
