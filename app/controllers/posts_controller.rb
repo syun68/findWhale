c3760276956e0b639b4bb0b9912607ab4b013a53
syun68 <coldsmash2628@gmail.com>
2022-11-03 10:23:40 +0900

    [update]

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :show, :edit_index, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
