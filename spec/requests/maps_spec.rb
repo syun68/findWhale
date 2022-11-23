b81bc18a8441384236d1b91e29a5a64dcf42f77c
syun68 <coldsmash2628@gmail.com>
2022-11-23 15:29:42 +0900

    [update] to add https test

--- spec/requests/maps_spec.rb
+++ spec/requests/maps_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Maps", type: :request do
-  describe "GoogleMap が表示されているページ" do
-    it "works! (now write some real specs)" do
-      get maps_path
-      expect(response).to have_http_status(200)
+  describe "GoogleMap が表示されているページへのアクセス" do
+    let!(:user) { create(:user) }
+    let!(:post) { create(:post, place_detail: '八丈島 底土港') }
+
+    context "GoogleMap の動作確認", js: true do
+      before do
+        get map_path(post.id)
+      end
+
+      it '正常なレスポンスが返されること' do
+        expect(response).to have_http_status(:success)
+      end 
     end
   end
 end
