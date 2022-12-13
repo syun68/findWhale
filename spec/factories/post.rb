bc9a41312a0ff51d1444c72fa82be8614eea367d
syun68 <coldsmash2628@gmail.com>
2022-12-13 14:24:06 +0900

    [update] to set necessary item

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -2,5 +2,14 @@ FactoryBot.define do
   factory :post do
     association :user
     title { Faker::Lorem.characters(number: 10) }
+    place_detail { '小笠原　父島' }
+    description { Faker::Lorem.characters(number: 10) }
+
+    after(:build) do |post|
+      post.image.attach(
+        io: File.open('spec/fixtures/post_test_image.jpg'),
+        filename: 'post_test_image.jpg', content_type: 'image/jpg'
+      )
+    end
   end
 end
