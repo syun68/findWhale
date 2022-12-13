ddf181736345e9d5403ae86b2511efbfaf4d6cae
syun68 <coldsmash2628@gmail.com>
2022-12-13 14:26:18 +0900

    [update] to change post factorybot

--- spec/requests/maps_spec.rb
+++ spec/requests/maps_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Maps', type: :request do
   describe 'GoogleMap が表示されているページへのアクセス' do
     let!(:user) { create(:user) }
-    let!(:post) { create(:post, place_detail: '八丈島 底土港') }
+    let!(:post) { create(:post) }
 
     context 'GoogleMap の動作確認', js: true do
       before do
