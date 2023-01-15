56bb5424e79dfa6b0d772ecf6e47cee96875e17e
syun68 <coldsmash2628@gmail.com>
2023-01-15 11:30:44 +0900

    [add] to add posts search function

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,12 @@
+class SearchController < ApplicationController
+
+  def search
+    gon.map_key = ENV['Google_Map_API']
+    @posts = Post.search(params[:keyword])
+      if @posts.count == 0
+        flash[:notice] = '検索結果がありませんでした'
+        redirect_to root_path
+      end
+  end
+
+end
