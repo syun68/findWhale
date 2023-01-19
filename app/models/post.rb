6ff559b0f7807297eec38fad95dcfd76e39b04dd
syun68 <coldsmash2628@gmail.com>
2023-01-19 11:06:20 +0900

    [update] 検索メソッドの作成のため

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,8 @@ class Post < ApplicationRecord
   geocoded_by :place_detail
   after_validation :geocode, if: :place_detail_changed?
 
+  scope :latest, -> {order(updated_at: :desc)}
+
   def self.search(keyword)
     posts = Post.where(['title LIKE ? OR description LIKE ? OR place_detail LIKE ?', "%#{keyword}%", "%#{keyword}%", "%#{keyword}%"])
   end
