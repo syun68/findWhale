eae9a14b0c77f69f8a22a41d7609ff5a201c3464
syun68 <coldsmash2628@gmail.com>
2023-01-26 18:02:22 +0900

    [update] to add logout test

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -3,15 +3,20 @@ require 'rails_helper'
 RSpec.describe 'Sessions', type: :system do
   let(:user) { create(:user) }
 
-  before do
-    visit login_path
-  end
-
   scenario 'ログインする' do
+    visit login_path
     fill_in 'session[email]', with: user.email
     fill_in 'session[password]', with: user.password
     click_button 'ログイン'
     expect(current_path).to eq root_path
     expect(page).to have_content "こんにちは。#{user.name}さん"
+  end
+
+  scenario 'ログアウトする' do
+    login
+    expect(current_path).to eq root_path
+    click_on 'ログアウト'
+    expect(current_path).to eq login_path
+    expect(page).to have_content 'ログアウトしました'
   end
 end
