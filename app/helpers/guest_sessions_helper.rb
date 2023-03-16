2cc527518ffff51ad16d3791705593169250fa3c
syun68 <coldsmash2628@gmail.com>
2023-03-16 21:32:20 +0900

    [update] helper内からインスタンス変数を削除するため表記変更

--- app/helpers/guest_sessions_helper.rb
+++ app/helpers/guest_sessions_helper.rb
@@ -1,7 +1,11 @@
 module GuestSessionsHelper
   def guest_check
-    if @current_user == User.find(100)
-      redirect_to root_path,notice: "ゲストユーザーにはこの機能はありません。"
-    end
+    redirect_to root_path, notice: 'ゲストユーザーにはこの機能はありません' if guest_user?
+  end
+
+  private
+
+  def guest_user?
+    session[:user_id] == 100
   end
 end
